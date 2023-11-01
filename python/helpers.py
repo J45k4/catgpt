7a9dce127ddd8a2706988e9ad62c3fd720777843
Jaska <pusanteemu@gmail.com>
2023-11-01 08:31:23 +0200

    Remove not used files

--- python/helpers.py
+++
@@ -1,6 +0,0 @@
-
-import os
-
-
-def getenv(key, default=0): return type(default)(os.getenv(key, default))
-DEBUG = getenv("DEBUG", 0)
--- python/whisper_test.py
+++
@@ -1,8 +0,0 @@
-import whisper
-import time
-
-model = whisper.load_model("base")
-now = time.time()
-result = model.transcribe("../workdir/turn_on_the_lights.wav")
-print(result["text"])
-print("Time taken: %f" % (time.time() - now))
