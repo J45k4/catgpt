843cedec61906356b7c23ee7cc03262151e88519
Jaska <pusanteemu@gmail.com>
2024-04-05 09:17:57 +0800

    Add minify to vite build config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		minify: true
+	},
 	preview: {
 		port: 7777,
 		host: "0.0.0.0"
