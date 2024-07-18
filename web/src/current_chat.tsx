937a871ef00550e22547c1b990e2a58333c66062
Jaska <pusanteemu@gmail.com>
2024-07-18 06:50:09 +0300

    implement latex rendering to chat message
    Still kind of buggy but it works i guess...

--- web/src/current_chat.tsx
+++ web/src/current_chat.tsx
@@ -1,14 +1,14 @@
 import { ws } from "./ws"
-import {formatDateTime } from "./utility"
+import { formatDateTime } from "./utility"
 import { Row } from "./layout"
 import { BiCopy } from "react-icons/bi"
 import { CodeBlock } from "react-code-blocks"
 import { cache, notifyChanges, useCache } from "./cache"
-import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
-import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
-import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin"
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
@@ -17,32 +17,33 @@ import { $createParagraphNode, $createTextNode, $getRoot, EditorState } from "le
 import { Loader } from "./common"
 import { FaRegStopCircle } from "react-icons/fa"
 import { TbReload } from "react-icons/tb";
+import Latex from "react-latex-next"
 
 function onError(error) {
-    console.error(error);
+	console.error(error);
 }
 
 const MsgEditorContnet = () => {
-    const { selectedBotId, selectedChatId, chatsGenerating } = useCache(cache => {
-        return {
-            selectedChatId: cache.selectedChatId,
-            selectedBotId: cache.selectedBotId,
+	const { selectedBotId, selectedChatId, chatsGenerating } = useCache(cache => {
+		return {
+			selectedChatId: cache.selectedChatId,
+			selectedBotId: cache.selectedBotId,
 			chatsGenerating: cache.chatsGenerating.has(cache.selectedChatId)
-        }
-    })
-    const [editor] = useLexicalComposerContext()
+		}
+	})
+	const [editor] = useLexicalComposerContext()
 
-    const sendMsg = useCallback(() => {
-        if (!cache.currentMsg) {
-            return
-        }
+	const sendMsg = useCallback(() => {
+		if (!cache.currentMsg) {
+			return
+		}
 
-        ws.send({
-            type: "SendMsg",
-            botId: selectedBotId,
-            txt: cache.currentMsg,
-            chatId: selectedChatId
-        })
+		ws.send({
+			type: "SendMsg",
+			botId: selectedBotId,
+			txt: cache.currentMsg,
+			chatId: selectedChatId
+		})
 
 		const chat = cache.chats.get(selectedChatId)
 
@@ -55,13 +56,13 @@ const MsgEditorContnet = () => {
 			})
 		}
 
-        cache.currentMsg = ""
-        notifyChanges()
-        editor.update(() => {
-            const root = $getRoot()
-            root.clear()
-        })
-    }, [editor, selectedBotId, selectedChatId])
+		cache.currentMsg = ""
+		notifyChanges()
+		editor.update(() => {
+			const root = $getRoot()
+			root.clear()
+		})
+	}, [editor, selectedBotId, selectedChatId])
 
 	const stopGenerating = useCallback(() => {
 		ws.send({
@@ -70,109 +71,109 @@ const MsgEditorContnet = () => {
 		})
 	}, [selectedChatId])
 
-    useLayoutEffect(() => {
-        const onKeyDown = (e: KeyboardEvent) => {
-            if (e.key === "Enter" && !e.shiftKey) {
-                sendMsg()
-            }
-        }
+	useLayoutEffect(() => {
+		const onKeyDown = (e: KeyboardEvent) => {
+			if (e.key === "Enter" && !e.shiftKey) {
+				sendMsg()
+			}
+		}
 
-        return editor.registerRootListener(
-            (
-                rootElement: null | HTMLElement,
-                prevRootElement: null | HTMLElement,
-            ) => {
-                if (prevRootElement !== null) {
-                    prevRootElement.removeEventListener('keydown', onKeyDown)
-                }
-                if (rootElement !== null) {
-                    rootElement.addEventListener('keydown', onKeyDown)
-                }
-            }
-        )
-    }, [editor, sendMsg])
+		return editor.registerRootListener(
+			(
+				rootElement: null | HTMLElement,
+				prevRootElement: null | HTMLElement,
+			) => {
+				if (prevRootElement !== null) {
+					prevRootElement.removeEventListener('keydown', onKeyDown)
+				}
+				if (rootElement !== null) {
+					rootElement.addEventListener('keydown', onKeyDown)
+				}
+			}
+		)
+	}, [editor, sendMsg])
 
-    return (
-        <div style={{
-            display: "flex",
+	return (
+		<div style={{
+			display: "flex",
 			flexDirection: "row",
-            border: "solid 1px rgba(0, 0, 0, 0.3)",
-            outline: "none",
-            padding: "0px"
-        }}>
-            <RichTextPlugin
-                contentEditable={<ContentEditable style={{
-                    flexGrow: 1,
-                    margin: "0px",
-                    padding: "10px",
-                    outline: "none",
-                }} />}
-                placeholder={<div></div>}
-                ErrorBoundary={LexicalErrorBoundary}
-            />
+			border: "solid 1px rgba(0, 0, 0, 0.3)",
+			outline: "none",
+			padding: "0px"
+		}}>
+			<RichTextPlugin
+				contentEditable={<ContentEditable style={{
+					flexGrow: 1,
+					margin: "0px",
+					padding: "10px",
+					outline: "none",
+				}} />}
+				placeholder={<div></div>}
+				ErrorBoundary={LexicalErrorBoundary}
+			/>
 			<div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-end" }}>
 				{!chatsGenerating && (
-				<button onClick={sendMsg}>
-					Send
-				</button>)}
+					<button onClick={sendMsg}>
+						Send
+					</button>)}
 				{chatsGenerating && (
 					<button onClick={stopGenerating}>
 						<FaRegStopCircle style={{ width: "20px", height: "20px" }} />
 					</button>
 				)}
 			</div>
-        </div>
+		</div>
 
-    )
+	)
 }
 
 export const SendMessageBox = () => {
-    const { currentMsg } = useCache(cache => {
-        return {
-            currentMsg: cache.currentMsg
-        }
-    })
+	const { currentMsg } = useCache(cache => {
+		return {
+			currentMsg: cache.currentMsg
+		}
+	})
 
-    const onMsgChange = useCallback((editorState: EditorState) => {
-        editorState.read(() => {
-            const root = $getRoot()
-            const text = root.getTextContent()
-            cache.currentMsg = text
-        })
-    }, [])
+	const onMsgChange = useCallback((editorState: EditorState) => {
+		editorState.read(() => {
+			const root = $getRoot()
+			const text = root.getTextContent()
+			cache.currentMsg = text
+		})
+	}, [])
 
-    return (
-        <div style={{ display: "flex" }}>
-            <div style={{ flexGrow: 1 }}>
-                <LexicalComposer initialConfig={{
-                    namespace: "NewMessageEditor",
-                    onError,
-                    theme: {
-                        paragraph: "editor-paragraph",
-                    },
-                    editorState(editor) {
-                        editor.update(() => {
-                            const root = $getRoot()
-                            currentMsg.split("\n").forEach(line => {
-                                const p = $createParagraphNode()
-                                const text = $createTextNode(line)
-                                p.append(text)
-                                root.append(p)
-                            })
-                        })
-                    },
-                    
-                }}>
-                    <MsgEditorContnet />
-                    <HistoryPlugin />
-                    <AutoFocusPlugin />
-                    <OnChangePlugin onChange={onMsgChange} />
+	return (
+		<div style={{ display: "flex" }}>
+			<div style={{ flexGrow: 1 }}>
+				<LexicalComposer initialConfig={{
+					namespace: "NewMessageEditor",
+					onError,
+					theme: {
+						paragraph: "editor-paragraph",
+					},
+					editorState(editor) {
+						editor.update(() => {
+							const root = $getRoot()
+							currentMsg.split("\n").forEach(line => {
+								const p = $createParagraphNode()
+								const text = $createTextNode(line)
+								p.append(text)
+								root.append(p)
+							})
+						})
+					},
 
-                </LexicalComposer>
-            </div>
+				}}>
+					<MsgEditorContnet />
+					<HistoryPlugin />
+					<AutoFocusPlugin />
+					<OnChangePlugin onChange={onMsgChange} />
 
-        </div>
-    )
+				</LexicalComposer>
+			</div>
+
+		</div>
+	)
 }
 
 export const EditMessageBox = (props: {
@@ -208,7 +209,7 @@ export const EditMessageBox = (props: {
 							})
 						})
 					},
-					
+
 				}}>
 					<MsgEditorContnet />
 					<HistoryPlugin />
@@ -224,160 +225,162 @@ export const EditMessageBox = (props: {
 export const CurrentChat = () => {
 	const [editing, setEditing] = useState(false)
 
-    const msgs = useCache(cache => {
+	const msgs = useCache(cache => {
 		if (!cache.selectedChatId) {
 			return []
 		}
 
-        const chat = cache.chats.get(cache.selectedChatId)
+		const chat = cache.chats.get(cache.selectedChatId)
 
 		if (!chat) {
 			return undefined
 		}
 
 		return chat.msgs
-    })
+	})
 
-    return (
-        <div className="segment">
-            <div>
+	return (
+		<div className="segment">
+			<div>
 				{!msgs && <Loader />}
-                {msgs?.map((msg, index) => {
-                    return (
-                        <div style={{ 
-                            display: "flex", 
-                            flexDirection: "column", 
-                            border: "solid 1px, #c4c4c4", 
-                            marginBottom: "20px",
-                            padding: "5px",
+				{msgs?.map((msg, index) => {
+					return (
+						<div style={{
+							display: "flex",
+							flexDirection: "column",
+							border: "solid 1px, #c4c4c4",
+							marginBottom: "20px",
+							padding: "5px",
 							paddingLeft: "10px",
-                            backgroundColor: index % 2 === 0 ? "#f6f6f6" : "white",
-                        }}
-                            key={msg.id}>
-                            <div style={{whiteSpace: "nowrap", display: "flex", flexWrap: "wrap" }}>
-                                <div style={{ flexGrow: 1, fontSize: "20px" }}>
-                                    {msg.user}
-                                </div>
-                                <div>
-                                    <Row style={{ flexWrap: "wrap" }}>
+							backgroundColor: index % 2 === 0 ? "#f6f6f6" : "white",
+						}}
+							key={msg.id}>
+							<div style={{ whiteSpace: "nowrap", display: "flex", flexWrap: "wrap" }}>
+								<div style={{ flexGrow: 1, fontSize: "20px" }}>
+									{msg.user}
+								</div>
+								<div>
+									<Row style={{ flexWrap: "wrap" }}>
 										<label style={{ marginRight: "10px", fontSize: "17px" }}>
 											{formatDateTime(msg.datetime)}
 										</label>
-                                        {/* Token Count:
+										{/* Token Count:
                                         {msg.tokenCount} */}
-                                        <BiCopy 
-                                            className="icon"
-                                            onClick={() => {
-                                                navigator.clipboard.writeText(msg.text)
-                                            }}
-                                        />
+										<BiCopy
+											className="icon"
+											onClick={() => {
+												navigator.clipboard.writeText(msg.text)
+											}}
+										/>
 										<TbReload className="icon" />
 										{/* <BiSolidEditAlt
 											style={{ fontSize: "25px", cursor: "pointer" }}
 											onClick={() => setEditing(true)} /> */}
-                                    </Row>    
-                                </div>
-                            </div>
+									</Row>
+								</div>
+							</div>
 							{editing &&
-							<EditMessageBox text={msg.text} />}
+								<EditMessageBox text={msg.text} />}
 							{!editing &&
-                            <ChatMessage
-                                key={msg.id}
-                                msgId={msg.id}
-                                text={msg.text}
-                                />}
-                        </div>
-                    )
-                })}
-                {/* <SendMessageBox chatId={chat?.id} /> */}
-            </div>
-        </div>
-    )
+								<ChatMessage
+									key={msg.id}
+									msgId={msg.id}
+									text={msg.text}
+								/>}
+						</div>
+					)
+				})}
+				{/* <SendMessageBox chatId={chat?.id} /> */}
+			</div>
+		</div>
+	)
 }
 
 export const ChatMessage = (props: {
-    msgId: string
-    text: string
+	msgId: string
+	text: string
 }) => {
-    const rows = []
+	const rows = []
 
-    let backbuffer = ""
-    let language = ""
-    let parsingCodeBlock = false
-    let parsingLanguage = false
-    let blockCount = 1
-    for (const char of props.text) {
-        backbuffer += char
+	let backbuffer = ""
+	let language = ""
+	let parsingCodeBlock = false
+	let parsingLanguage = false
+	let blockCount = 1
+	for (const char of props.text) {
+		backbuffer += char
 
-        if (parsingCodeBlock) {
-            if (backbuffer.endsWith("```")) {
-                const code = backbuffer.slice(0, backbuffer.length - 3)
+		if (parsingCodeBlock) {
+			if (backbuffer.endsWith("```")) {
+				const code = backbuffer.slice(0, backbuffer.length - 3)
 
-                rows.push(
-                    <div style={{ marginBottom: "15px" }}>
-                        <div style={{ display: "flex" }}>
-                            <div style={{ flexGrow: 1 }}>
-                                {language}
-                            </div>
-                            <div>
-                                <BiCopy style={{ margin: "5px", fontSize: "20px", cursor: "pointer" }}
-                                    onClick={() => navigator.clipboard.writeText(code)} />
-								
-                            </div>
-                        </div>
-                        
-                        <CodeBlock key={blockCount++} text={code}
-                            language={language} 
-                            
-                            />
-                    </div>   
-                )
-                backbuffer = ""
+				rows.push(
+					<div style={{ marginBottom: "15px" }}>
+						<div style={{ display: "flex" }}>
+							<div style={{ flexGrow: 1 }}>
+								{language}
+							</div>
+							<div>
+								<BiCopy style={{ margin: "5px", fontSize: "20px", cursor: "pointer" }}
+									onClick={() => navigator.clipboard.writeText(code)} />
+
+							</div>
+						</div>
+
+						<CodeBlock key={blockCount++} text={code}
+							language={language}
+
+						/>
+					</div>
+				)
+				backbuffer = ""
 				parsingCodeBlock = false
-            }
-            continue
-        }
+			}
+			continue
+		}
 
-        if (parsingLanguage) {
-            if (backbuffer.endsWith("\n")) {
-                language = backbuffer.slice(0, backbuffer.length - 1)
-                parsingLanguage = false
-                parsingCodeBlock = true
-                backbuffer = ""
-            }
+		if (parsingLanguage) {
+			if (backbuffer.endsWith("\n")) {
+				language = backbuffer.slice(0, backbuffer.length - 1)
+				parsingLanguage = false
+				parsingCodeBlock = true
+				backbuffer = ""
+			}
 
-            continue
-        }
+			continue
+		}
 
-        if (backbuffer.endsWith("```")) {
-            parsingLanguage = true
-            backbuffer = ""
-            continue
-        }
+		if (backbuffer.endsWith("```")) {
+			parsingLanguage = true
+			backbuffer = ""
+			continue
+		}
 
-        if (char === "\n") {
-            rows.push(
-                <div key={blockCount++} style={{ whiteSpace: "pre-wrap" }}>
-                    {backbuffer}
-                </div>
-            )
+		if (char === "\n") {
+			rows.push(
+				<div key={blockCount++} style={{ whiteSpace: "pre-wrap" }}>
+					<Latex>
+						{backbuffer}
+					</Latex>
+				</div>
+			)
 
-            backbuffer = ""
-            continue
-        }
-    }
+			backbuffer = ""
+			continue
+		}
+	}
 
-    if (backbuffer !== "") {
-        rows.push(
-            <div key={blockCount++} style={{ whiteSpace: "pre-wrap" }}>
-                {backbuffer}
-            </div>
-        )
-    }
+	if (backbuffer !== "") {
+		rows.push(
+			<div key={blockCount++} style={{ whiteSpace: "pre-wrap" }}>
+				{backbuffer}
+			</div>
+		)
+	}
 
-    return (
-        <div style={{ display: "flex", flexDirection: "column", overflow: "auto" }} onTouchStart={e => e.stopPropagation()}>
-            {rows}
-        </div>
-    )
+	return (
+		<div style={{ display: "flex", flexDirection: "column", overflow: "auto" }} onTouchStart={e => e.stopPropagation()}>
+			{rows}
+		</div>
+	)
 }
